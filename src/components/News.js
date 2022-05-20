5954d1b9db856d22a73cd97a4e9148fe7cb40023
Sai Saketh <konjerlasaisaketh2002@gmail.com>
2022-05-20 22:36:04 +0530

    Added Spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,56 +1,60 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner';
 export class News extends Component {
     
-    constructor(){
+    constructor(props){
         super();
         this.state={
             a: [],
-            load:false,
+            load:true,
             page:1,
             totalResults:70
         }
     }
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b5249a0b35074894a618085074273439&page=1&pagesize=20"
+        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b5249a0b35074894a618085074273439&page=1&pagesize=${this.props.pagesize}`
         let data = await fetch(url);
         let parseddata= await data.json();
-        this.setState({a:parseddata.articles})
+        this.setState({a:parseddata.articles,totalResults:parseddata.totalResults,load:false})
     }
 
     handleuponclick = async ()=>{
-        if(Math.ceil((this.state.totalResults)/20)<this.state.page+1){
+        if(Math.ceil((this.state.totalResults)/(this.props.pagesize)<(this.state.page+1))){
 
         }else{
         let url = `
-        https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b5249a0b35074894a618085074273439&page-${this.state.page+1}&pagesize=20`;
+        https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b5249a0b35074894a618085074273439&page=${this.state.page+1}&pagesize=${this.props.pagesize}`;
+        this.setState({load:true})
         let data = await fetch(url);
         let parseddata= await data.json();
-        this.setState({a:parseddata.articles,page:this.state.page+1})
+        this.setState({a:parseddata.articles,page:this.state.page+1,load:false})
         }
     }
 
     handleonpclick = async ()=>{
         let url = `
-        https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b5249a0b35074894a618085074273439&page-${this.state.page-1}&pagesize=20`;
+        https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b5249a0b35074894a618085074273439&page=${this.state.page-1}&pagesize=${this.props.pagesize}`;
+        this.setState({load:true})
         let data = await fetch(url);
         let parseddata= await data.json();
-        this.setState({a:parseddata.articles,page:this.state.page-1})
+        this.setState({a:parseddata.articles,page:this.state.page-1,load:false})
     } 
   render() {
     return (
       <div className='container my-3'>
-          <h2>NewsMonkey - Top Headlines</h2>
+          <h2 className='text-center'>NewsMonkey - Top Headlines</h2>
+          {this.state.load && <Spinner/>}
           <div className='row'>
-              {this.state.a.map((element)=>{
-                 return <div className='col-md-4 my-1' key={element.url}>
+              {!this.state.load && this.state.a.map((element)=>{
+                 return <div className='col-md-3 my-1' key={element.url}>
                  <Newsitem  title={element.title?element.title:""} desc={element.description?element.description:" "} imageUrl={element.urlToImage} newsUrl={element.url}/>
                  </div> 
               })}
           </div>
           <div  className='container d-flex justify-content-between'>
               <button disabled={this.state.page<=1} type='button' onClick={this.handleonpclick} className='btn btn-dark'>&larr;Previous</button>
-              <button type='button' onClick={this.handleuponclick} className='btn btn-dark'>Next &rarr;</button>
+              <button type='button' disabled={(this.state.totalResults)/(this.props.pagesize)<this.state.page+1} onClick={this.handleuponclick} className='btn btn-dark'>Next &rarr;</button>
           </div>
       </div>
     )
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -4,13 +4,13 @@ export class Newsitem extends Component {
   render() {
       let {title,desc,imageUrl,newsUrl}= this.props;
     return (
-    <div className='my-3'>
+    <div className='my-4'>
       <div className="card" style={{width: "18rem"}}>
         <img src={!imageUrl?"https://img.huffingtonpost.com/asset/62855afd1e0000abe71b4fb5.jpeg?cache=qRkk5tFrpD&ops=1778_1000":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h6 className="card-title">{title}</h6>
           <p className="card-text">{desc}</p>
-          <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
+          <a    rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
         </div>
       </div>
       </div>
